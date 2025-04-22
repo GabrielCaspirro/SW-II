2b9369b46e95b6eecfc68f294d60468bf033dc74
Gabriel Caspirro Demarchi <124791810+GabrielCaspirro@users.noreply.github.com>
2025-04-22 14:40:32 -0300

    Add files via upload

--- Aula06/api.php
+++ Aula06/api.php
@@ -1,5 +1,5 @@
 <?php
-    header("Content-Type: application/json");
+    header("Content-Type: application/json; charset=UTF-8");
 
     $metodo = $_SERVER['REQUEST_METHOD'];
 
@@ -14,20 +14,48 @@
     switch($metodo){
         case 'GET':
             //echo "Ações do método GET ";
-            echo json_encode($usuarios, JSON_PRETTY_PRINT || JSON_UNESCAPED_UNICODE);
+            
+            if(isset($_GET["id"])){
+                $id = intval($_GET["id"]);
+                $usuario_encontrado = null;
+
+                foreach($usuarios as $usuario){
+                    if($usuario['id'] == $id){
+                        $usuario_encontrado = $usuario;
+                        break;
+                    }
+                }
+
+                if($usuario_encontrado){
+                    echo json_encode($usuario_encontrado, JSON_PRETTY_PRINT || JSON_UNESCAPED_UNICODE);
+                }
+                else{
+                    http_response_code(400);
+                    echo json_encode(["erro" => "Usuário não encontrado"], JSON_UNESCAPED_UNICODE);
+                }
+            }else{
+                echo json_encode($usuarios, JSON_PRETTY_PRINT || JSON_UNESCAPED_UNICODE);
+            }
+
             break;
         case 'POST':
             //echo "Ações do método POST";
             $dados = json_decode(file_get_contents('php://input'), true);
 
-            if(!isset($dados["id"]) || !isset($dados["nome"]) || !isset($dados["email"])){
+            if(!isset($dados["nome"]) || !isset($dados["email"])){
                 http_response_code(400);
                 echo json_encode(["erro" => "Dados incompletos."], JSON_UNESCAPED_UNICODE);
                 exit;
             }
 
+            $novo_id = 1;
+            if(!empty($usuarios)){
+                $ids = array_column($usuarios, "id");
+                $novo_id = max($ids) + 1;
+            }
+
             $novoUsuario = [
-                "id" => $dados["id"],
+                "id" => $novo_id,
                 "nome" => $dados["nome"],
                 "email" => $dados["email"]
             ];
@@ -39,8 +67,8 @@
 
             break;
         default:
-            http_response_code(400);
-            echo json_encode(["erro" => "Dados incompletos."], JSON_UNESCAPED_UNICODE);
+            http_response_code(405);
+            echo json_encode(["erro" => "Método não permitido."], JSON_UNESCAPED_UNICODE);
             break;
     }
 ?>
